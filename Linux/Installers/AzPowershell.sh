6b54caeb69fef13d96b61db18fa4cd2dbf10dd1d
Ben Origas <borigas@ocuvera.com>
2026-08-19 15:00:35 -0500

    Linux az pwsh setup

--- Linux/Installers/AzPowershell.sh
+++ Linux/Installers/AzPowershell.sh
@@ -9,4 +9,10 @@ fi
 echo "Installing Azure PowerShell module (Az) for current user..."
 pwsh -NoLogo -NoProfile -Command "Set-PSRepository PSGallery -InstallationPolicy Trusted; Install-Module Az -Scope CurrentUser -Repository PSGallery -Force -AllowClobber"
 
+echo "Configuring Azure PowerShell auth for Linux (disable WAM broker)..."
+pwsh -NoLogo -NoProfile -Command 'Update-AzConfig -EnableLoginByWam $false -Scope CurrentUser -AppliesTo Az'
+
+echo "Verifying Az module install..."
+pwsh -NoLogo -NoProfile -Command "Get-Module Az -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1 Name,Version"
+
 echo "Azure PowerShell install complete."
